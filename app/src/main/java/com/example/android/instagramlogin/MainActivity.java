1fe0271120081b92b155b7923a53bc8cbee25c7d
deepdhar <dhar2017.slg@gmail.com>
2021-05-18 13:49:04 +0530

    updated UI background

--- app/src/main/java/com/example/android/instagramlogin/MainActivity.java
+++ app/src/main/java/com/example/android/instagramlogin/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.android.instagramlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.main_relativeLayout);
+        animationDrawable = (AnimationDrawable)relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
+    }
+
 }
